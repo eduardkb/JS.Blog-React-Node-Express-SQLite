ee8a98f5f4ca511a09cac9cac0a13160443c6b39
eduardkb <eduardbuhali@gmail.com>
2023-03-16 15:58:57 -0300

    changed header structure

--- aFrontend/src/components/Header.js
+++ aFrontend/src/components/Header.js
@@ -13,91 +13,10 @@ import MenuItem from '@mui/material/MenuItem';
 import Tooltip from '@mui/material/Tooltip';
 
 
-
 function Header(props) {
 	const dta = props.data
 
-	function XsCategoryMenu(props) {
-		const categories = props.categories
-		const setCatFilter = props.funcCatFilter
 
-		const [anchorEl, setAnchorEl] = React.useState(null);
-		const open = Boolean(anchorEl);
-		const handleClick = (event) => {
-			setAnchorEl(event.currentTarget);
-		};
-		const handleClose = () => {
-			setAnchorEl(null);
-		};
-		const handleBtnCatClick = (e, id, setFunc) => {
-			e.preventDefault();
-			setFunc(id)
-		}
-		return (
-			<React.Fragment>
-				<Box>
-					<Typography variant="h6" sx={{ margin: 0 }}>
-						Categories:
-					</Typography>
-					<Tooltip title="Categories">
-						<MenuIcon
-							onClick={handleClick}
-							size="small"
-							sx={{ ml: 2 }}
-							aria-controls={open ? 'account-menu' : undefined}
-							aria-haspopup="true"
-							aria-expanded={open ? 'true' : undefined}
-						>
-							<Avatar sx={{ width: 32, height: 32 }}>M</Avatar>
-						</MenuIcon>
-					</Tooltip>
-				</Box>
-				<Menu
-					anchorEl={anchorEl}
-					id="account-menu"
-					open={open}
-					onClose={handleClose}
-					onClick={handleClose}
-					PaperProps={{
-						elevation: 0,
-						sx: {
-							overflow: 'visible',
-							filter: 'drop-shadow(0px 2px 8px rgba(0,0,0,0.32))',
-							mt: 1.5,
-							'& .MuiAvatar-root': {
-								width: 32,
-								height: 32,
-								ml: -0.5,
-								mr: 1,
-							},
-							'&:before': {
-								content: '""',
-								display: 'block',
-								position: 'absolute',
-								top: 0,
-								right: 14,
-								width: 10,
-								height: 10,
-								bgcolor: 'background.paper',
-								transform: 'translateY(-50%) rotate(45deg)',
-								zIndex: 0,
-							},
-						},
-					}}
-					transformOrigin={{ horizontal: 'right', vertical: 'top' }}
-					anchorOrigin={{ horizontal: 'right', vertical: 'bottom' }}
-				>
-					{categories.map((category) => {
-						return (
-							<MenuItem key={category.id} onClick={e => handleBtnCatClick(e, category.id, setCatFilter)}>
-								{category.name}
-							</MenuItem>
-						)
-					})}
-				</Menu>
-			</React.Fragment>
-		);
-	}
 
 	function BuildCatMenu(props) {
 		const categories = props.categories
@@ -107,6 +26,99 @@ function Header(props) {
 			setFunc(id)
 		}
 
+		function SmCategoryMenu(props) {
+			const categories = props.categories
+			const setCatFilter = props.funcCatFilter
+
+			categories.map((category) => {
+				return (
+					<Button key={category.id} onClick={e => handleBtnCatClick(e, category.id, setCatFilter)} size="small" variant="contained" sx={{ bgcolor: "secondary.main", marginLeft: 1, marginBottom: 1 }}>
+						{category.name}
+					</Button>
+				)
+			})
+		}
+		function XsCategoryMenu(props) {
+			const categories = props.categories
+			const setCatFilter = props.funcCatFilter
+
+			const [anchorEl, setAnchorEl] = React.useState(null);
+			const open = Boolean(anchorEl);
+			const handleClick = (event) => {
+				setAnchorEl(event.currentTarget);
+			};
+			const handleClose = () => {
+				setAnchorEl(null);
+			};
+			return (
+				<React.Fragment>
+					<Box>
+						<Typography variant="h6" sx={{ margin: 0 }}>
+							Categories:
+						</Typography>
+						<Tooltip title="Categories">
+							<MenuIcon
+								onClick={handleClick}
+								size="small"
+								sx={{ ml: 2 }}
+								aria-controls={open ? 'account-menu' : undefined}
+								aria-haspopup="true"
+								aria-expanded={open ? 'true' : undefined}
+							>
+								<Avatar sx={{ width: 32, height: 32 }}>M</Avatar>
+							</MenuIcon>
+						</Tooltip>
+					</Box>
+					<Menu
+						anchorEl={anchorEl}
+						id="account-menu"
+						open={open}
+						onClose={handleClose}
+						onClick={handleClose}
+						PaperProps={{
+							elevation: 0,
+							sx: {
+								overflow: 'visible',
+								filter: 'drop-shadow(0px 2px 8px rgba(0,0,0,0.32))',
+								mt: 1.5,
+								'& .MuiAvatar-root': {
+									width: 32,
+									height: 32,
+									ml: -0.5,
+									mr: 1,
+								},
+								'&:before': {
+									content: '""',
+									display: 'block',
+									position: 'absolute',
+									top: 0,
+									right: 14,
+									width: 10,
+									height: 10,
+									bgcolor: 'background.paper',
+									transform: 'translateY(-50%) rotate(45deg)',
+									zIndex: 0,
+								},
+							},
+						}}
+						transformOrigin={{ horizontal: 'right', vertical: 'top' }}
+						anchorOrigin={{ horizontal: 'right', vertical: 'bottom' }}
+					>
+						<MenuItem key={0} onClick={e => handleBtnCatClick(e, 0, setCatFilter)}>
+							All Categories
+						</MenuItem>
+						{categories.map((category) => {
+							return (
+								<MenuItem key={category.id} onClick={e => handleBtnCatClick(e, category.id, setCatFilter)}>
+									{category.name}
+								</MenuItem>
+							)
+						})}
+					</Menu>
+				</React.Fragment>
+			);
+		}
+
 		if (props.buildType === 'xs') {
 			return (
 				<XsCategoryMenu categories={categories} funcCatFilter={setCatFilter} />
@@ -114,13 +126,12 @@ function Header(props) {
 		}
 		if (props.buildType === 'sm') {
 			return (
-				categories.map((category) => {
-					return (
-						<Button key={category.id} onClick={e => handleBtnCatClick(e, category.id, setCatFilter)} size="small" variant="contained" sx={{ bgcolor: "secondary.main", marginLeft: 1, marginBottom: 1 }}>
-							{category.name}
-						</Button>
-					)
-				})
+				<>
+					{/* <Button key={0} onClick={e => handleBtnCatClick(e, 0, setCatFilter)} size="small" variant="contained" sx={{ bgcolor: "secondary.main", marginLeft: 1, marginBottom: 1 }}>
+						All
+					</Button> */}
+					<SmCategoryMenu categories={categories} funcCatFilter={setCatFilter} />
+				</>
 			)
 		}
 
