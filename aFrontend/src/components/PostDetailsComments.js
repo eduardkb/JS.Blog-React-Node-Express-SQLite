464b872c14f383b744d67ecb1d631571af150eb1
eduardkb <eduardbuhali@gmail.com>
2023-03-21 12:54:12 -0300

    Finished improve comment display on post details

--- aFrontend/src/components/PostDetailsComments.js
+++ aFrontend/src/components/PostDetailsComments.js
@@ -1,11 +1,39 @@
 import React from "react";
 import Box from "@mui/material/Box";
+import Typography from "@mui/material/Typography";
+import Divider from '@mui/material/Divider';
 
 export default function PostDetailsComments(props) {
     const comments = props.comm;
-    return (
-        <Box sx={{ p: 5 }}>
-            Comments: {comments[0].comment}
-        </Box>
-    )
+    const filteredComments = comments.filter((comment) => {
+        return (comment.published = true)
+    })
+
+    if (filteredComments.length > 0) {
+        return (
+            <Box sx={{ m: 2, p: 2, bgcolor: "white", outlineStyle: "dashed" }}>
+                <Typography variant="h5" display="block">
+                    Comments:
+                </Typography>
+                <Box sx={{ ml: 5 }}>
+                    {filteredComments.map((comment) => {
+                        return (
+                            <div>
+                                <Typography variant="body2" display="block">
+                                    User: {comment.userId}
+                                </Typography>
+                                <Typography variant="h6" display="block">
+                                    Comment: {comment.comment}
+                                </Typography>
+                                <Typography variant="body2" display="block">
+                                    Date: {new Date(comment.createdAt).toLocaleDateString('pt-BR')}
+                                </Typography>
+                                <Divider sx={{ m: 2 }} />
+                            </div>
+                        )
+                    })}
+                </Box>
+            </Box >
+        )
+    }
 }
