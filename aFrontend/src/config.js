ef52f21edba65d8bf5b25e2f0dee805591c425bf
eduardkb <95878832+eduardkb@users.noreply.github.com>
2023-03-22 16:24:47 -0300

    changed axios URL to cloud

--- aFrontend/src/config.js
+++ aFrontend/src/config.js
@@ -1,5 +1,6 @@
 const GLOBAL_SETTINGS = {
-    axiosUrl: "http://127.0.0.1:8080/api",
+    // axiosUrl: "http://127.0.0.1:8080/api",
+    axiosUrl: "https://dev.eduardkb.website/api",
 }
 
 export default GLOBAL_SETTINGS;
