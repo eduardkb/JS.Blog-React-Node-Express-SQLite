232f0058c856bc20f8097034de6db28ba6b04502
eduardkb <eduardbuhali@gmail.com>
2023-03-23 08:30:30 -0300

    Bug fix - categories buttons not filtering and returning to posts

--- aFrontend/src/components/Header/Filter.js
+++ aFrontend/src/components/Header/Filter.js
@@ -17,6 +17,200 @@ export default function Filter(props) {
     const dta = props.dta
     const setPostSelected = props.setPostSelected
 
+
+    function BuildCatMenu(props) {
+        const categories = props.categories
+        const { setCatFilter } = useContext(PostDataContext);
+
+        const handleBtnCatClick = (e, id, setFunc) => {
+            e.preventDefault();
+            setPostSelected(0)
+            setFunc(id)
+        }
+
+        function SmCategoryMenu(props) {
+            const categories = props.categories
+            const setCatFilter = props.funcCatFilter
+            return (
+                <React.Fragment>
+                    <Button key={0} onClick={e => handleBtnCatClick(e, 0, setCatFilter)} size="small" variant="contained" sx={{ bgcolor: "secondary.main", marginLeft: 1, marginBottom: 1 }}>
+                        All
+                    </Button>
+                    {categories.map((category) => {
+                        return (
+                            <Button key={category.id} onClick={e => handleBtnCatClick(e, category.id, setCatFilter)} size="small" variant="contained" sx={{ bgcolor: "secondary.main", marginLeft: 1, marginBottom: 1 }}>
+                                {category.name}
+                            </Button>
+                        )
+                    })}
+                </React.Fragment>
+
+            )
+        }
+        function XsCategoryMenu(props) {
+            const categories = props.categories
+            const setCatFilter = props.funcCatFilter
+
+            const [anchorEl, setAnchorEl] = React.useState(null);
+            const open = Boolean(anchorEl);
+            const handleClick = (event) => {
+                setAnchorEl(event.currentTarget);
+            };
+            const handleClose = () => {
+                setAnchorEl(null);
+            };
+            return (
+                <React.Fragment>
+                    <Box >
+                        <Typography display="inline" variant="h6" sx={{ margin: 0, verticalAlign: "middle" }}>
+                            Categories:
+                        </Typography>
+                        <Tooltip title="Categories">
+                            <MenuIcon
+                                sx={{ verticalAlign: "middle" }}
+                                color="text"
+                                onClick={handleClick}
+                                fontSize="large"
+                                aria-controls={open ? 'account-menu' : undefined}
+                                aria-haspopup="true"
+                                aria-expanded={open ? 'true' : undefined}
+                            >
+                            </MenuIcon>
+                        </Tooltip>
+                    </Box>
+                    <Menu
+                        anchorEl={anchorEl}
+                        id="account-menu"
+                        open={open}
+                        onClose={handleClose}
+                        onClick={handleClose}
+                        PaperProps={{
+                            elevation: 0,
+                            sx: {
+                                overflow: 'visible',
+                                filter: 'drop-shadow(0px 2px 8px rgba(0,0,0,0.32))',
+                                mt: 1.5,
+                                '& .MuiAvatar-root': {
+                                    width: 32,
+                                    height: 32,
+                                    ml: -0.5,
+                                    mr: 1,
+                                },
+                                '&:before': {
+                                    content: '""',
+                                    display: 'block',
+                                    position: 'absolute',
+                                    top: 0,
+                                    right: 14,
+                                    width: 10,
+                                    height: 10,
+                                    bgcolor: 'background.paper',
+                                    transform: 'translateY(-50%) rotate(45deg)',
+                                    zIndex: 0,
+                                },
+                            },
+                        }}
+                        transformOrigin={{ horizontal: 'right', vertical: 'top' }}
+                        anchorOrigin={{ horizontal: 'right', vertical: 'bottom' }}
+                    >
+                        <MenuItem key={0} onClick={e => handleBtnCatClick(e, 0, setCatFilter)}>
+                            All Categories
+                        </MenuItem>
+                        {categories.map((category) => {
+                            return (
+                                <MenuItem key={category.id} onClick={e => handleBtnCatClick(e, category.id, setCatFilter)}>
+                                    {category.name}
+                                </MenuItem>
+                            )
+                        })}
+                    </Menu>
+                </React.Fragment>
+            );
+        }
+
+        if (props.buildType === 'xs') {
+            return (
+                <XsCategoryMenu categories={categories} funcCatFilter={setCatFilter} />
+            )
+        }
+        if (props.buildType === 'sm') {
+            return (
+                <SmCategoryMenu categories={categories} funcCatFilter={setCatFilter} />
+            )
+
+        }
+
+    }
+
+    function FilterSlide() {
+        const [anchorEl, setAnchorEl] = React.useState(null);
+
+        const handleClick = (event) => {
+            setAnchorEl(event.currentTarget);
+        };
+
+        const handleClose = () => {
+            setAnchorEl(null);
+        };
+        const { titleFilter, setTitleFilter, fResetFilters } = useContext(PostDataContext)
+        function handleChangePostFilter(e) {
+            setTitleFilter(e.target.value);
+        }
+        function handleBtnClearFilters(e) {
+            fResetFilters()
+            setTitleFilter(e.target.value);
+        }
+
+        const open = Boolean(anchorEl);
+        const id = open ? 'simple-popover' : undefined;
+
+        return (
+            <div>
+                <Button aria-describedby={id}
+                    variant="contained" onClick={handleClick}
+                    endIcon={<ArrowDropDownIcon />} sx={{ marginLeft: 2 }}
+                >
+                    Filters
+                </Button>
+                <Popover
+                    id={id}
+                    open={open}
+                    anchorEl={anchorEl}
+                    onClose={handleClose}
+                    anchorOrigin={{
+                        vertical: 'bottom',
+                        horizontal: 'left',
+                    }}
+                >
+                    {/* ITEMS ON POPOVER MENU */}
+                    <Grid container direction="column" p="10px">
+                        <TextField
+                            id="filterPostTitle"
+                            label="Filter by Post Title"
+                            variant="standard"
+                            color="tertiary"
+                            sx={{ ml: 1, mb: 1 }}
+                            onChange={handleChangePostFilter}
+                            value={titleFilter}
+                        />
+                        <TextField
+                            id="filterDate"
+                            label="Filter by Start Date"
+                            variant="standard"
+                            color="tertiary"
+                            sx={{ ml: 1, mb: 1 }}
+                        />
+                        <Button vaiant="contained" onClick={handleBtnClearFilters}
+                            sx={{ backgroundColor: "primary.main", color: "black" }}
+                        >
+                            Clear Filters
+                        </Button>
+                    </Grid>
+                </Popover>
+            </div >
+        );
+    }
+
     if (dta !== undefined) {
         return (
             <>
@@ -44,196 +238,3 @@ export default function Filter(props) {
     }
 }
 
-function BuildCatMenu(props) {
-    const categories = props.categories
-    const setPostSelected = props.setPostSelected
-    const { setCatFilter } = useContext(PostDataContext);
-
-    const handleBtnCatClick = (e, id, setFunc) => {
-        e.preventDefault();
-        setPostSelected(0)
-        setFunc(id)
-    }
-
-    function SmCategoryMenu(props) {
-        const categories = props.categories
-        const setCatFilter = props.funcCatFilter
-        return (
-            <React.Fragment>
-                <Button key={0} onClick={e => handleBtnCatClick(e, 0, setCatFilter)} size="small" variant="contained" sx={{ bgcolor: "secondary.main", marginLeft: 1, marginBottom: 1 }}>
-                    All
-                </Button>
-                {categories.map((category) => {
-                    return (
-                        <Button key={category.id} onClick={e => handleBtnCatClick(e, category.id, setCatFilter)} size="small" variant="contained" sx={{ bgcolor: "secondary.main", marginLeft: 1, marginBottom: 1 }}>
-                            {category.name}
-                        </Button>
-                    )
-                })}
-            </React.Fragment>
-
-        )
-    }
-    function XsCategoryMenu(props) {
-        const categories = props.categories
-        const setCatFilter = props.funcCatFilter
-
-        const [anchorEl, setAnchorEl] = React.useState(null);
-        const open = Boolean(anchorEl);
-        const handleClick = (event) => {
-            setAnchorEl(event.currentTarget);
-        };
-        const handleClose = () => {
-            setAnchorEl(null);
-        };
-        return (
-            <React.Fragment>
-                <Box >
-                    <Typography display="inline" variant="h6" sx={{ margin: 0, verticalAlign: "middle" }}>
-                        Categories:
-                    </Typography>
-                    <Tooltip title="Categories">
-                        <MenuIcon
-                            sx={{ verticalAlign: "middle" }}
-                            color="text"
-                            onClick={handleClick}
-                            fontSize="large"
-                            aria-controls={open ? 'account-menu' : undefined}
-                            aria-haspopup="true"
-                            aria-expanded={open ? 'true' : undefined}
-                        >
-                        </MenuIcon>
-                    </Tooltip>
-                </Box>
-                <Menu
-                    anchorEl={anchorEl}
-                    id="account-menu"
-                    open={open}
-                    onClose={handleClose}
-                    onClick={handleClose}
-                    PaperProps={{
-                        elevation: 0,
-                        sx: {
-                            overflow: 'visible',
-                            filter: 'drop-shadow(0px 2px 8px rgba(0,0,0,0.32))',
-                            mt: 1.5,
-                            '& .MuiAvatar-root': {
-                                width: 32,
-                                height: 32,
-                                ml: -0.5,
-                                mr: 1,
-                            },
-                            '&:before': {
-                                content: '""',
-                                display: 'block',
-                                position: 'absolute',
-                                top: 0,
-                                right: 14,
-                                width: 10,
-                                height: 10,
-                                bgcolor: 'background.paper',
-                                transform: 'translateY(-50%) rotate(45deg)',
-                                zIndex: 0,
-                            },
-                        },
-                    }}
-                    transformOrigin={{ horizontal: 'right', vertical: 'top' }}
-                    anchorOrigin={{ horizontal: 'right', vertical: 'bottom' }}
-                >
-                    <MenuItem key={0} onClick={e => handleBtnCatClick(e, 0, setCatFilter)}>
-                        All Categories
-                    </MenuItem>
-                    {categories.map((category) => {
-                        return (
-                            <MenuItem key={category.id} onClick={e => handleBtnCatClick(e, category.id, setCatFilter)}>
-                                {category.name}
-                            </MenuItem>
-                        )
-                    })}
-                </Menu>
-            </React.Fragment>
-        );
-    }
-
-    if (props.buildType === 'xs') {
-        return (
-            <XsCategoryMenu categories={categories} funcCatFilter={setCatFilter} />
-        )
-    }
-    if (props.buildType === 'sm') {
-        return (
-            <SmCategoryMenu categories={categories} funcCatFilter={setCatFilter} />
-        )
-
-    }
-
-}
-
-function FilterSlide() {
-    const [anchorEl, setAnchorEl] = React.useState(null);
-
-    const handleClick = (event) => {
-        setAnchorEl(event.currentTarget);
-    };
-
-    const handleClose = () => {
-        setAnchorEl(null);
-    };
-    const { titleFilter, setTitleFilter, fResetFilters } = useContext(PostDataContext)
-    function handleChangePostFilter(e) {
-        setTitleFilter(e.target.value);
-    }
-    function handleBtnClearFilters(e) {
-        fResetFilters()
-        setTitleFilter(e.target.value);
-    }
-
-    const open = Boolean(anchorEl);
-    const id = open ? 'simple-popover' : undefined;
-
-    return (
-        <div>
-            <Button aria-describedby={id}
-                variant="contained" onClick={handleClick}
-                endIcon={<ArrowDropDownIcon />} sx={{ marginLeft: 2 }}
-            >
-                Filters
-            </Button>
-            <Popover
-                id={id}
-                open={open}
-                anchorEl={anchorEl}
-                onClose={handleClose}
-                anchorOrigin={{
-                    vertical: 'bottom',
-                    horizontal: 'left',
-                }}
-            >
-                {/* ITEMS ON POPOVER MENU */}
-                <Grid container direction="column" p="10px">
-                    <TextField
-                        id="filterPostTitle"
-                        label="Filter by Post Title"
-                        variant="standard"
-                        color="tertiary"
-                        sx={{ ml: 1, mb: 1 }}
-                        onChange={handleChangePostFilter}
-                        value={titleFilter}
-                    />
-                    <TextField
-                        id="filterDate"
-                        label="Filter by Start Date"
-                        variant="standard"
-                        color="tertiary"
-                        sx={{ ml: 1, mb: 1 }}
-                    />
-                    <Button vaiant="contained" onClick={handleBtnClearFilters}
-                        sx={{ backgroundColor: "primary.main", color: "black" }}
-                    >
-                        Clear Filters
-                    </Button>
-                </Grid>
-            </Popover>
-        </div >
-    );
-}
