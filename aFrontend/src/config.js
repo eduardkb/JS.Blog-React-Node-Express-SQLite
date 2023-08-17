f335bd8ca17dfd9f445935df286a28f325de073d
eduardkb <eduardbuhali@gmail.com>
2023-08-17 09:55:35 -0300

    test FE Axios URL

--- aFrontend/src/config.js
+++ aFrontend/src/config.js
@@ -1,6 +1,6 @@
 const GLOBAL_SETTINGS = {
-    // axiosUrl: "http://127.0.0.1:8080/api",
-    axiosUrl: "https://studynodewebapp-wa.azurewebsites.net",
+    axiosUrl: "http://127.0.0.1:8080/api",
+    // axiosUrl: "https://studynodewebapp-wa.azurewebsites.net",
 }
 
 export default GLOBAL_SETTINGS;
