b59af23a26286df2359a78328bbc653596f705cd
eduardkb <eduardbuhali@gmail.com>
2023-08-17 11:33:27 -0300

    fixed frontend api URL

--- aFrontend/src/config.js
+++ aFrontend/src/config.js
@@ -1,6 +1,6 @@
 const GLOBAL_SETTINGS = {
     // axiosUrl: "http://127.0.0.1:8080/api",
-    axiosUrl: "https://studynodewebapp-wa.azurewebsites.net",
+    axiosUrl: "https://studynodewebapp-wa.azurewebsites.net/api",
 }
 
 export default GLOBAL_SETTINGS;
